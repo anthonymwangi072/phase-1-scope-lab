a8e79f8bec15736fa65bd86b1fd20ec15729cb3d
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-18 18:56:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = "BOB";
+
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = "george";
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "kevin";
+}
